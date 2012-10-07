bdb4edd48cc13c248e6130c32cba55e9c6cec4e4
Benjamin Lévêque <benjamin@leveque.me>
2012-10-07 23:20:42 +0200

    add support for the package control

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,23 +2,27 @@
 #
 # class { 'sublime-text2':
 #   source => 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1.dmg',
-#   preferences => '/Users/benji07/.sublime-settings',
-#   cmdLine => true
+#   cmdLine => true,
 # }
-class sublime-text2 ($source, $preferences = undef, $cmdLine = false) {
+class sublime-text2 ($source, $cmdLine = false) {
     package { 'Sublime Text 2':
         provider => appdmg,
         ensure => present,
         source => $source
     }
 
-    if $preferences != undef {
-        file { "/Users/${id}/Library/Application Support/Sublime Text 2/Packages/User/Preferences.sublime-settings":
-            ensure => link,
-            force => true,
-            target => $preferences,
-            require => Package['Sublime Text 2']
-        }
+    file { "/Users/${id}/Library/Application Support/Sublime Text 2/Packages/User/Preferences.sublime-settings":
+        ensure => link,
+        force => true,
+        target => '/Users/${id}/.sublime-settings',
+        require => Package['Sublime Text 2']
+    }
+
+    file { "/Users/${id}/Library/Application Support/Sublime Text 2/Packages/User/Package Control.sublime-settings":
+        ensure => link,
+        force => true,
+        target => '/Users/${id}/.sublime-packages',
+        require => Package['Sublime Text 2']
     }
 
     if $cmdLine {
@@ -28,10 +32,16 @@ class sublime-text2 ($source, $preferences = undef, $cmdLine = false) {
             target => '/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl'
         }
     }
+
+    file { "Package Control.sublime-package":
+        path => "/Users/${id}/Library/Application Support/Sublime Text 2/Packages/Package Control.sublime-package"
+        ensure => present,
+        source => http://sublime.wbond.net/Package%20Control.sublime-package,
+        require => Package['Sublime Text 2'],
+    }
 }
 
 class {'sublime-text2':
     source => 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1.dmg',
-    preferences => '/Users/benji07/.sublime-settings',
     cmdLine => true
 }
